022c5af50fd83a98c2b08cbaa42f1ee824775343
Ben Nguyen <bennguyenn24@gmail.com>
2023-02-11 12:28:18 -0800

    Updated Skills

---
+++ PERSONAL-PORTFOLIO/src/portfolio components/Projects.jsx
@@ -0,0 +1,37 @@
+import { Container, Row, Col } from "react-bootstrap";
+const Projects = () => {
+//     {
+//         title: "Business Startup",
+//         description: "Design & Development",
+//         imgUrl: {},
+//     },
+//     {
+//         title: "Business Startup",
+//         description: "Design & Development",
+//         imgUrl: {},
+//     },
+//     {
+//         title: "Business Startup",
+//         description: "Design & Development",
+//         imgUrl: {},
+//     },
+//     {
+//         title: "Business Startup",
+//         description: "Design & Development",
+//         imgUrl: {}
+//     },
+// }
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>Projects</h2>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export default Projects;

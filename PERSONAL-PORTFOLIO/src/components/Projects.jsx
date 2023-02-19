b641f04b020826c5ba049a74cd9ca2892152214f
Ben Nguyen <bennguyenn24@gmail.com>
2023-02-18 22:10:59 -0800

    Added Contact section

--- PERSONAL-PORTFOLIO/src/components/Projects.jsx
+++ PERSONAL-PORTFOLIO/src/components/Projects.jsx
@@ -32,7 +32,8 @@ const Projects = () => {
 						<Tab.Container id="projects-tabs" defaultActiveKey="first">
 							<Nav
 								variant="pills"
-								className="nav-pills mb-5 justify-content-center align-items-center "
+								className="nav-pills mb-5 justify-content-center justify-content-center align-items-center" 
+								id="pills-tab"
 							>
 								<Nav.Item>
 									<Nav.Link eventKey="first">Tab One</Nav.Link>

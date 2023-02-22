e5b30f276f698b8f55220b62bc21907b523cc0a6
Ben Nguyen <bennguyenn24@gmail.com>
2023-02-22 12:22:45 -0800

    Created Client / Server folders

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+// const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// const contactEmail = nodemailer.createTransport({
+// 	service: "gmail",
+// 	auth: {
+// 		user: process.env.ADMIN_USER,
+// 		pass: process.env.EMAIL_PASS,
+// 	},
+// });
+
+// contactEmail.verify((error) => {
+// 	if (error) {
+// 		console.log(error);
+// 	} else {
+// 		console.log("Ready to Send");
+// 	}
+// });
+
+// app.post("/contact", (req, res) => {
+//   const name = req.body.firstName + req.body.lastName;
+//   const email = req.body.email;
+//   const message = req.body.message;
+//   const phone = req.body.phone;
+//   const mail = {
+//     from: name,
+//     to: "********@gmail.com",
+//     subject: "Contact Form Submission - Portfolio",
+//     html: `<p>Name: ${name}</p>
+//            <p>Email: ${email}</p>
+//            <p>Phone: ${phone}</p>
+//            <p>Message: ${message}</p>`,
+//   };
+//   contactEmail.sendMail(mail, (error) => {
+//     if (error) {
+//       res.json(error);
+//     } else {
+//       res.json({ code: 200, status: "Message Sent" });
+//     }
+//   });
+// });
+
+app.listen(5000, () => console.log("Server Running"));

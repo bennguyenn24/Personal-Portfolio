47f7b5babccde00543dc996edd164563b0ec02aa
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-07 18:50:29 -0700

    Fixed Banner bug

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,82 +1,92 @@
-import './Banner.css'
+import "./Banner.css";
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import topG from "../img/topg.png";
+import logo4 from "../img/logo4.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
 const Banner = () => {
-	const [loopNum, setLoopNum] = useState(0);
-	const [isDeleting, setIsDeleting] = useState(false);
-	const toRotate = ["Web Developer", "Software Engineer"];
-	const [text, setText] = useState("");
-	const [delta, setDelta] = useState(300 - Math.random() * 100);
-	const period = 2000;
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Web Developer", "Software Engineer"];
+    const [text, setText] = useState("");
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
 
-	useEffect(() => {
-		let ticker = setInterval(() => {
-			tick();
-		}, delta);
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
 
-		return () => {
-			clearInterval(ticker);
-		};
-	}, [text]);
+        return () => {
+            clearInterval(ticker);
+        };
+    }, [text]);
 
-	const tick = () => {
-		let i = loopNum % toRotate.length;
-		let fullText = toRotate[i];
-		let updatedText = isDeleting
-			? fullText.substring(0, text.length - 1)
-			: fullText.substring(0, text.length + 1);
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting
+            ? fullText.substring(0, text.length - 1)
+            : fullText.substring(0, text.length + 1);
 
-		setText(updatedText);
+        setText(updatedText);
 
-		if (isDeleting) {
-			setDelta((prevDelta) => prevDelta / 2);
-		}
+        if (isDeleting) {
+            setDelta((prevDelta) => prevDelta / 2);
+        }
 
-		if (!isDeleting && updatedText === fullText) {
-			setIsDeleting(true);
-			setDelta(period);
-		} else if (isDeleting && updatedText === "") {
-			setIsDeleting(false);
-			setLoopNum(loopNum + 1);
-			setDelta(500);
-		}
-	};
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === "") {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    };
 
-	return (
-		<section className="banner" id="home">
-			<Container>
-				<Row className="align-items-center">
-					<Col xs={12} md={6} xl={7}>
-						<TrackVisibility>
-							{({ isVisible }) => (
-								<div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-									<h1>
-										{"BEN NGUYEN"}
-										<span className="wrap"> {text}</span>
-									</h1>
-									<p>
-                                        Hi there! I am a <br/>
-										Full Stack Software Engineer (ReactJS/ExpressJS).
-									</p>
-									<button onClick={() => console.log("connect")}>
-										Let's Connect <ArrowRightCircle size={25} />
-									</button>
-								</div>
-							)}
-						</TrackVisibility>
-					</Col>
-					<Col xs={12} md={6} xl={5}>
-						<img src={topG} alt="Header Img" />
-					</Col>
-				</Row>
-			</Container>
-		</section>
-	);
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <TrackVisibility>
+                            {({ isVisible }) => (
+                                <div
+                                    className={
+                                        isVisible
+                                            ? "animate__animated animate__fadeIn"
+                                            : ""
+                                    }
+                                >
+                                    <h1>
+                                        {"BEN NGUYEN"}
+                                        <span className="wrap"> {text}</span>
+                                    </h1>
+                                    <p>
+                                        Hi there! I am a <br />
+                                        Full Stack Software Engineer
+                                        (ReactJS/ExpressJS).
+                                    </p>
+                                    <button
+                                        onClick={() => console.log("connect")}
+                                    >
+                                        Let's Connect{" "}
+                                        <ArrowRightCircle size={25} />
+                                    </button>
+                                </div>
+                            )}
+                        </TrackVisibility>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={logo4} alt="Header Img" className="logo-image" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
 };
 
 export default Banner;

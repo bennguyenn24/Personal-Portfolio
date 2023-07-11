9ce7bbd9012b78fcc44db054892f73ffae9295ab
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-10 22:32:05 -0700

    Changed Navbar responsiveness

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,60 +1,90 @@
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
-import './Navbar.css'
-import logo from '../img/BN.png'
-import navIcon1 from '../img/email.svg'
-import navIcon2 from '../img/github.svg'
-import navIcon3 from '../img/linkedin.svg'
+import './Navbar.css';
+import logo from '../img/BN.png';
+import navIcon1 from '../img/email.svg';
+import navIcon2 from '../img/github.svg';
+import navIcon3 from '../img/linkedin.svg';
 
- const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        }
-        window.addEventListener('scroll', onScroll);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', onScroll);
 
-        return () => window.removeEventListener('scroll', onScroll)
-    }, []);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-    }
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
   return (
-    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+    <Navbar expand="lg" className={scrolled ? 'scrolled' : ''} sticky="top">
       <Container>
         <Navbar.Brand href="#home">
-            <img src={logo} alt="" />
+          <img src={logo} alt="" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"> 
-        <span className='navbar-toggler-icon'></span>
-        </Navbar.Toggle>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>About</Nav.Link>
-            <Nav.Link href="#skill"className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects"className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            <Nav.Link href="#contact"className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')}>Contact Me</Nav.Link>
+            <Nav.Link
+              href="#about"
+              className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'}
+              onClick={() => onUpdateActiveLink('about')}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
+              onClick={() => onUpdateActiveLink('skills')}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
+              onClick={() => onUpdateActiveLink('projects')}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'}
+              onClick={() => onUpdateActiveLink('contact')}
+            >
+              Contact Me
+            </Nav.Link>
           </Nav>
-          <span className='navbar-text'>
-            <div className='social-icon'>
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="" />
+              </a>
             </div>
-            <button className='' onClick={() => console.log('connect')}><span>Let's connect</span></button>
+            <button className="" onClick={() => console.log('connect')}>
+              <span>Let's connect</span>
+            </button>
           </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavBar;

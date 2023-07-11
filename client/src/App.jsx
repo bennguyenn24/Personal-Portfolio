ac9206fd7b3406be5a10e3f3daa92f8079c302f0
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-10 23:16:49 -0700

    Limited width scrolling on mobile devices

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,14 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="container">
       <Banner />
       <About />
       <Skills />
       <Projects />
       <Contact />
       <Footer />
+    </div>
     </div>
   )
 }
